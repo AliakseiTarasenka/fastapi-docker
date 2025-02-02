6c7f44432905c28fb90d23c0f95896495ee7ae7d
Aliaksei Tarasenka <aliaksei_tarasenka@epam.com>
2025-02-02 21:17:50 +0100

    add server initial lifespan

--- pyjobs/web/server.py
+++ pyjobs/web/server.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
-
 from pyjobs.web.api.api import router as jobs_router
+from contextlib import asynccontextmanager
+
+# the lifespan event
+@asynccontextmanager
+async def lifespan(server: FastAPI):
+    print("Server is starting...")
+    yield
+    print("server is stopping")
 
 version = "v1"
-name = "Jobs"
 server = FastAPI(debug=True,
-                 title=name,
-                 description=f"A RESTful API for a {name} review web service",
-                 version=version)
-
-server.include_router(jobs_router, prefix=f"/api/{version}", tags=[name])
+                 title='Jobs',
+                 description='A RESTful API for a Jobs review web service',
+                 version=version,
+                 lifespan=lifespan
+                 )
+server.include_router(jobs_router, prefix=f"/api/{version}", tags=['jobs'])
