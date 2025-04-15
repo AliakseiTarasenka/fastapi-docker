a760210a8968b9fe844eac82fcf8f36e6a8bd593
Aliaksei Tarasenka <aliaksei_tarasenka@epam.com>
2025-04-15 18:39:35 +0200

    Update CRUD logic for books

--- main.py
+++ main.py
@@ -1,46 +1,115 @@
-from fastapi import FastAPI, Header
+from fastapi import FastAPI,status
+from fastapi.exceptions import HTTPException
 from pydantic import BaseModel
-from typing import Optional
+from typing import List
 
-class BookCreateModel(BaseModel):
+class Book(BaseModel):
+    id: int
     title: str
     author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
+
+class BookUpdateModel(BaseModel):
+    title: str
+    author: str
+    publisher: str
+    page_count: int
+    language: str
 
 app = FastAPI()
 
-@app.post("/book")
-async def create_book(book_data: BookCreateModel):
-    return {
-        "title": book_data.title,
-        "author": book_data.author
-    }
+books = [
+    {
+        "id": 1,
+        "title": "Think Python",
+        "author": "Allen B. Downey",
+        "publisher": "O'Reilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 1234,
+        "language": "English",
+    },
+    {
+        "id": 2,
+        "title": "Django By Example",
+        "author": "Antonio Mele",
+        "publisher": "Packt Publishing Ltd",
+        "published_date": "2022-01-19",
+        "page_count": 1023,
+        "language": "English",
+    },
+    {
+        "id": 3,
+        "title": "The web socket handbook",
+        "author": "Alex Diaconu",
+        "publisher": "Xinyu Wang",
+        "published_date": "2021-01-01",
+        "page_count": 3677,
+        "language": "English",
+    },
+    {
+        "id": 4,
+        "title": "Head first Javascript",
+        "author": "Hellen Smith",
+        "publisher": "Oreilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 540,
+        "language": "English",
+    },
+    {
+        "id": 5,
+        "title": "Algorithms and Data Structures In Python",
+        "author": "Kent Lee",
+        "publisher": "Springer, Inc",
+        "published_date": "2021-01-01",
+        "page_count": 9282,
+        "language": "English",
+    },
+    {
+        "id": 6,
+        "title": "Head First HTML5 Programming",
+        "author": "Eric T Freeman",
+        "publisher": "O'Reilly Media",
+        "published_date": "2011-21-01",
+        "page_count": 3006,
+        "language": "English",
+    },
+]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/books", response_model=List[Book])
+async def get_books():
+    return books
 
-@app.get("/greet")
-async def greet(name: Optional[str] = "User", age: int = 0) -> dict:
-    return {"message": f"Hello {name}", "age": age}
+@app.get("/books/{id}", response_model=Book)
+async def get_book(id: int) ->dict:
+    for book in books:
+        if book["id"] == id:
+            return book
 
-# request query parameter: http://127.0.0.1:8000/greet?age=10
-# request path parameter: http://127.0.0.1:8000/greet/{name}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
+@app.patch("/books/{id}", response_model=Book)
+async def update_book(id: int, update_data: BookUpdateModel) ->dict:
+    for book in books:
+        if book["id"] == id:
+            book.update(update_data.model_dump())
+            return book
 
-@app.get("/headers")
-async def headers(
-        user_agent: Optional[str] = Header(None),
-        accept_encoding: Optional[str] = Header(None),
-        referer: Optional[str] = Header(None),
-        connection: Optional[str] = Header(None),
-        accept_language: Optional[str] = Header(None),
-        host: Optional[str] = Header(None)):
-     request_headers = {}
-     request_headers["User-Agent"] = user_agent
-     request_headers["Accept-Encoding"] = accept_encoding
-     request_headers["Referer"] = referer
-     request_headers["Accept-Language"] = accept_language
-     request_headers["Connection"] = connection
-     request_headers["Host"] = host
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
-     return request_headers
+@app.delete("/books/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(id: int):
+    for book in books:
+        if book["id"] == id:
+            books.remove(book)
+            return
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+
+@app.post("/books", status_code=status.HTTP_201_CREATED, response_model=Book)
+async def create_book(book_data: Book) ->dict:
+    new_book = book_data.model_dump()
+    books.append(new_book)
+    return new_book
