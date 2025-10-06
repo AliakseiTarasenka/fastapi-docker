50991df72e90e1f5e18e4a261e28b2e5ac00a783
Aliaksei Tarasenka <Aliaksei_Tarasenka@epam.com>
2025-10-06 18:50:32 +0200

    add authentication endpoint to refresh token

--- src/web/main.py
+++ src/web/main.py
@@ -3,7 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from src.db.database import init_db
 from src.web.routes.books import app as books_router
-from src.web.routes.users import app as auth_router
+from src.web.routes.auth import app as auth_router
 from src.web.routes.users import app as users_router
 
 
