749e925c105082bf1cb06297f978fd7f27daff81
Aliaksei Tarasenka <Aliaksei_Tarasenka@epam.com>
2025-10-07 23:59:14 +0200

    add authorization logic

---
+++ src/service/authorization.py
@@ -0,0 +1,30 @@
+from sqlmodel.ext.asyncio.session import AsyncSession
+from typing import Any, List
+from src.db.database import get_session
+from src.models.users import User
+from src.service.errors import AccountNotVerified, InsufficientPermission
+from src.service.authentication import AccessTokenBearer
+from fastapi import Depends
+
+class RoleChecker:
+    def __init__(self, allowed_roles: List[str]) -> None:
+        self.allowed_roles = allowed_roles
+
+    def __call__(self, current_user: User = Depends(get_current_user)) -> Any:
+        if not current_user.is_verified:
+            raise AccountNotVerified()
+        if current_user.role in self.allowed_roles:
+            return True
+
+        raise InsufficientPermission()
+
+
+async def get_current_user(
+    token_details: dict = Depends(AccessTokenBearer()),
+    session: AsyncSession = Depends(get_session),
+):
+    user_email = token_details["user"]["email"]
+
+    user = await user_service.get_user_by_email(user_email, session)
+
+    return user
